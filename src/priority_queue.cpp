508619c828ea35b90727f458cd32f05f454db97e
Joseph McGovern <joseph.mcgovern@webfilings.com>
2017-04-11 16:42:14 -0500

    size_t instead of int

--- src/priority_queue.cpp
+++ src/priority_queue.cpp
@@ -12,7 +12,7 @@ void PriorityQueue :: insertWithPriority(Character * character, int priority) {
         nodes.push_back(node);
         return;
     }
-    for (int i = 0; i < nodes.size(); i++) {
+    for (size_t i = 0; i < nodes.size(); i++) {
         Node existing_node = nodes[i];
         if (priority <= existing_node.priority) {
             nodes.insert(nodes.begin() + i, node);
@@ -29,10 +29,7 @@ Node PriorityQueue :: extractMin() {
 }
 
 void PriorityQueue :: decreasePriority(Character * character, int priority) {
-    Node node;
-    node.character = character;
-    node.priority = priority;
-    for (int i = 0; i < nodes.size(); i++) {
+    for (size_t i = 0; i < nodes.size(); i++) {
         Node existing_node = nodes[i];
         if (character->id.compare(existing_node.character->id) == 0) {
             nodes.erase(nodes.begin() + i);
@@ -46,7 +43,7 @@ void PriorityQueue :: decreaseCoordPriority(struct Coordinate coord, int priorit
     Node node;
     node.coord = coord;
     node.priority = priority;
-    for (int i = 0; i < nodes.size(); i++) {
+    for (size_t i = 0; i < nodes.size(); i++) {
         Node existing_node = nodes[i];
         if (existing_node.coord.x == coord.x && existing_node.coord.y == coord.y) {
             nodes.erase(nodes.begin() + i);
@@ -66,7 +63,7 @@ void PriorityQueue :: insertCoordWithPriority(struct Coordinate coord, int prior
         return;
     }
     bool was_added = false;
-    for (int i = 0; i < nodes.size(); i++) {
+    for (size_t i = 0; i < nodes.size(); i++) {
         Node existing_node = nodes[i];
         if (priority <= existing_node.priority) {
             nodes.insert(nodes.begin() + i, node);
